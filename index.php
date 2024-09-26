ef0d891a63d648cb87e3c5a56aa0c74cb40f4766
PapaZola! <155160291+Renold0@users.noreply.github.com>
2024-09-26 14:29:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Latihan Form 2</title>
-</head>
-<body>
-    <h2>FORM INPUT IDENTITAS MAHASISWA</h2>
-    
-    <form method="post" action="tampildata.php">
-        NPM:<br>
-        <input type="text" name="txtnpm" size="20"><br><br>
-        
-        NAMA:<br>
-        <input type="text" name="txtnama" size="20"><br><br>
-        
-        JENIS KELAMIN:<br>
-        <input type="radio" name="txtjeniskelamin" value="aki-aki" checked>aki-aki<br>
-        <input type="radio" name="txtjeniskelamin" value="nini-nini">nini-nini<br><br>
-        
-        HOBI:<br>
-        <input type="checkbox" name="txthobi[]" value="Nontonlagi" checked>Nontonlagi<br>
-        <input type="checkbox" name="txthobi[]" value="Nonton">Nonton<br><br>
-        
-        <input type="submit" name="btnsubmit" value="Tampil">
-    </form>
-</body>
-</html>
+<?php
+echo "hello world";
+?>
