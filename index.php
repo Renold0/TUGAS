bd670f9b02dd4f74e2790e3b8355ba5c954b3da3
Renold0 <auah301@gmail.com>
2024-09-26 14:16:04 +0700

    file baru

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$npm = isset($_POST['txtnpm']) ? $_POST['txtnpm'] : '';
+$nama = isset($_POST['txtnama']) ? $_POST['txtnama'] : '';
+$jeniskelamin = isset($_POST['txtjeniskelamin']) ? $_POST['txtjeniskelamin'] : '';
+$hobi = isset($_POST['txthobi']) ? $_POST['txthobi'] : [];
+
+echo "------------------------<br>";
+echo "Identitas Mahasiswa<br>";
+echo "------------------------<br>";
+echo "NPM : " . htmlspecialchars($npm) . "<br>";
+echo "Nama : " . htmlspecialchars($nama) . "<br>";
+echo "Jenis Kelamin : " . htmlspecialchars($jeniskelamin) . "<br>";
+
+if (!empty($hobi)) {
+    echo "Hobi : " . implode(", ", array_map('htmlspecialchars', $hobi)) . "<br>";
+} else {
+    echo "Hobi : Tidak ada hobi yang dipilih<br>";
+}
+?>
